c0b281dbf8e081551dedee34ddb90509d457e841
reza <h.reza.az.92@gmail.com>
2023-12-20 12:17:41 +0330

    email adnd user name and password confirm

--- account/forms.py
+++ account/forms.py
@@ -6,7 +6,10 @@ from django.core.exceptions import ValidationError
 class UserRegistrationForm(forms.Form):
     username = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(label='password', widget=forms.PasswordInput(
+        attrs={'class': 'form-control', 'placeholder': 'Enter yor password'}))
+    password2 = forms.CharField(label='confirm password', widget=forms.PasswordInput(
+        attrs={'class': 'form-control', 'placeholder': 'Enter confirm password'}))
 
     def clean_email(self):
         email = self.email
@@ -15,4 +18,17 @@ class UserRegistrationForm(forms.Form):
             raise ValidationError('This email already exists')
         return email
 
-            
+    def clean_username(self):
+        username = self.username
+        user = User.objects.filter(username=username)
+        if user:
+            raise ValidationError('This username already exists')
+        return username
+
+    def clean(self):  # cleaned and validating fields that depend on each other
+        cd = super().clean()
+        p1 = cd.get('password')
+        p2 = cd.get('password2')
+
+        if p1 and p2 and p1 != p2:
+            raise ValidationError('passwords do not match')
